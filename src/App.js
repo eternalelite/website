3849f0a29ea2906f1258640832dfbb0b348200cb
Poovamraj T T <poovamraj@gmail.com>
2020-09-29 17:24:34 +0530

    completed navigation issue

--- src/App.js
+++ src/App.js
@@ -1,43 +1,23 @@
-import React,{Component} from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-import ReactDom from "react-dom";
 
-import {BrowserRouter as Router,Route,Link,withRouter,Switch} from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from './components/navbar';
 import Home from './components/home';
-import Image from './components/image';
 import AboutUs from './components/about';
 import Gallery from './components/gallery';
-function App(){
-  
-  return ( <div><Router><Navbar />
-  
-    <Route exact path="/" >
-     <Home /> 
-       
-    </Route>
-    <Route exact path="/about" component={AboutUs}> 
-    
-    </Route>
-    <Route exact path="/gallery"> 
-      <Gallery />
-    </Route>
-   </Router>
-    </div>);
-}
-function About(){
-  return(<AboutUs />);
-}
+function App() {
 
-// ReactDom.render((
-//    <Router>
-//     <Switch>
-//     <Route exact path="/" component={withRouter(App)} /> 
-//      <Route exact path="/about" component={withRouter(About)} />
-//      </Switch>
-    
-//     </Router>
-// ),document.getElementById("root"));
+  return (<div>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={AboutUs} />
+        <Route exact path="/gallery" component={Gallery} />
+      </Switch>
+    </Router>
+  </div>);
+}
 
 export default App;
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,75 +1,52 @@
-import React, { Component } from 'react';
-import {BrowserRouter,Route,Link} from "react-router-dom";
+import React from 'react';
+import { useHistory } from "react-router-dom";
 import AppBar from '@material-ui/core/AppBar';
-import { Box, makeStyles, Grid, Container, Toolbar } from '@material-ui/core';
-import {Typography,IconButton,Button} from '@material-ui/core'
+import { Box, Toolbar } from '@material-ui/core';
+import { IconButton } from '@material-ui/core'
 
-const style=theme=>({
-  root:{
-    flexGrow:1
+const style = theme => ({
+  root: {
+    flexGrow: 1
   },
-    logo:{
-     color:"#007F82",
-     fontFamily:"harlow solid",
-     fontSize:"15pt",
-            
-        
-    },
-    navLi:{
-        color:"#007F82",
-        fontFamily:"Segoe UI",
-        font:"bold",
-        margin:"15px",
-        fontSize:"12pt"
-    }
-    
+  logo: {
+    color: "#007F82",
+    fontFamily: "harlow solid",
+    fontSize: "15pt",
+  },
+  navLi: {
+    color: "#007F82",
+    fontFamily: "Segoe UI",
+    font: "bold",
+    margin: "15px",
+    fontSize: "12pt",
+    cursor: 'pointer'
+  },
+  navbarcl: { 
+    border: "none", 
+    backgroundColor: "#EEE3E7", 
+    boxShadow: "none", 
+    width: "100%", 
+    overflowX: "hidden", 
+    margin: "0px" 
+  }
 });
-const st=style();
-class navbar extends Component{
-    
-    render(){
-        const navbarcl={border:"none",backgroundColor:"#EEE3E7" ,boxShadow:"none" ,width:"100%",overflowX:"hidden",margin:"0px"};
-        return (<AppBar position="static" style={navbarcl}>
-        <Toolbar>
-          <IconButton edge="start"  color="inherit" aria-label="menu">
-            
-          </IconButton>
-          <Box flexGrow="1">
-          <BrowserRouter><Link to="/">
-          <Typography variant="h6" style={st.logo} >
-        
-            Eeternal Elite
-            
-          </Typography>
-          </Link>
-            </BrowserRouter>
-          </Box>
-          <BrowserRouter><Link to="/">
-          <Typography variant="h6" style={st.navLi} >
-            Home 
-          </Typography>
-          </Link>
-            </BrowserRouter>
-            <BrowserRouter><Link to="/gallery">
-          <Typography variant="h6" style={st.navLi} >
-            Gallery
-            
-          </Typography>
-          </Link></BrowserRouter>
-          <BrowserRouter><Link to="/about">
-
-          <Typography variant="h6" style={st.navLi} >
-            About US
-          </Typography>
-          </Link>
-            </BrowserRouter>
-
-          <Typography variant="h6" style={st.navLi} >
-            Reach 
-          </Typography>
-        </Toolbar>
-      </AppBar>
-        );  
-    }
+const st = style();
+function NavBar() {
+  let history = useHistory()
+  
+  return (
+    <AppBar position="static" style={st.navbarcl}>
+      <Toolbar>
+        <IconButton edge="start" color="inherit" aria-label="menu" />
+        <Box flexGrow="1">
+          <h6 style={st.logo} >Eeternal Elite</h6>
+        </Box>
+        <h6 style={st.navLi} onClick={()=>{history.push(`/`)}}> Home </h6>
+        <h6 style={st.navLi} onClick={()=>{history.push(`/gallery`)}}>Gallery</h6>
+        <h6 style={st.navLi} onClick={()=>{history.push(`/about`)}}>About US</h6>
+        <h6 style={st.navLi} >Reach</h6>
+      </Toolbar>
+    </AppBar>
+  )
 }
-export default  navbar;
+export default NavBar;
